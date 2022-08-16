3a6c22fa61ccdb7f1fcc26d6190c3c7d43b30f98
Carlos Moctezuma <crdgom@gmail.com>
2022-08-15 21:43:34 -0500

    fisrt commit

---
+++ a.js
@@ -0,0 +1,13 @@
+
+
+//funcion mayor de edad
+function mayorDeEdad(edad){
+    if (edad>=18)
+        console.log("Mayor de edad");
+    else{
+        console.log("Menor de edad");
+    }
+}
+
+mayorDeEdad(21);
+mayorDeEdad(6);
