ba04ed7f017d5bbf85af42c990f73c3e71dacbf5
Carlos Moctezuma <crdgom@gmail.com>
2022-08-20 13:31:50 -0500

    Finished Pinterest pink section HTML & CSS, implemented js functionality

--- Modulo-1--Proyectos/Clon-Pinterest/js/script.js
+++ Modulo-1--Proyectos/Clon-Pinterest/js/script.js
@@ -1,3 +1,4 @@
+// variable para insertar texto 
 const ideas = [
     "actividad para niños",
     "idea para una cena especial",
@@ -5,10 +6,24 @@ const ideas = [
     "proyecto de bricolaje"
 ]
 
-function ideaRotator(){
-    const el = document.getElementById("ideas");
-    ideas.forEach(() => {
-        el.innerHtml(ideas)
-    
-    });
+//funcion para mostrar texto cada segundo en orden
+
+function ideaRotator() {
+    for (let i = 0; i < ideas.length; i++) {
+        setTimeout(function () {
+            document.getElementById("ideas").innerHTML = ideas[i];
+            // cambiar el color de cada idea
+            if (i == 0) {
+                document.getElementById("ideas").style.color = "rgb(0, 118, 211)";
+            } else if (i == 1) {
+                document.getElementById("ideas").style.color = "rgb(97, 140, 123)";
+            } else if (i == 2) {
+                document.getElementById("ideas").style.color = "rgb(194, 139, 0)";
+            } else if (i == 3) {
+                document.getElementById("ideas").style.color = "rgb(64, 122, 87)";
+            }
+        }, 1500 * i);
+    }
+    // volver a empezar
+    setTimeout(ideaRotator, 1500 * ideas.length);
 }
