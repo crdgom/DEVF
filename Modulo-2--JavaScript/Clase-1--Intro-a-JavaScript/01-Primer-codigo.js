92c443171d36a6c351a8d8f1765effde4e5decaf
Carlos Moctezuma <crdgom@gmail.com>
2022-09-14 17:13:01 -0500

    in & out data and variable definition

--- Modulo-2--JavaScript/Clase-1--Intro-a-JavaScript/01-Primer-codigo.js
+++ Modulo-2--JavaScript/Clase-1--Intro-a-JavaScript/01-Primer-codigo.js
@@ -1,11 +1,170 @@
 // # Salidas y Entradas
 
+/*
+    *************************
+    ************************* 
+    ************************* 
+            SALIDAS
+    ************************* 
+    ************************* 
+    ************************* 
+*/
+
 // # console.log()
 
 console.log("Hello World");
 console.log(2+4);
 console.log(1>0);
 
-var name = "Carlos";
+// # document.write()
 
-console.log(name);
+// !let salida = document.write("Hello World");
+
+// # alert()
+
+// !alert("Hello World");
+
+/*
+    *************************
+    ************************* 
+    ************************* 
+            ENTRADAS
+    ************************* 
+    ************************* 
+    ************************* 
+*/
+
+// # prompt()
+
+// !prompt("Escribe tu nombre");
+
+// # Variables y Constantes
+
+/*
+    *************************
+    ************************* 
+    ************************* 
+        var (Deprecated)
+    ************************* 
+    ************************* 
+    ************************* 
+*/
+
+// # Numeros
+
+var number = 2;
+var decimal = 5.9;
+
+console.log(number);
+console.log(decimal);
+
+// # Cadenas de texto
+
+var string = "Hello world";
+
+console.log(string);
+
+// # Booleanos
+var boolean = true;
+
+console.log(boolean);
+
+/*
+    *************************
+    ************************* 
+    ************************* 
+               let
+    ************************* 
+    ************************* 
+    ************************* 
+*/
+
+// # Numeros
+
+let numberOne = 5;
+let decimalOne = 5.9;
+
+console.log(numberOne);
+console.log(decimalOne);
+
+// # Cadenas de texto
+
+let stringOne = "Hello world";
+
+console.log(stringOne);
+
+// # Booleanos
+let booleanOne = true;
+
+console.log(booleanOne);
+
+/*
+    *************************
+    ************************* 
+    ************************* 
+           constantes
+    ************************* 
+    ************************* 
+    ************************* 
+*/
+
+// # Numeros
+
+const CUADRADO = 4;
+const PI = 3.1415;
+
+console.log(CUADRADO);
+console.log(PI);
+
+// # Cadenas de texto
+
+const MODULO = "Kata 02 Intro a Javascript";
+
+console.log(MODULO);
+
+// # Booleanos
+const APRENDIENDO = true;
+
+console.log(APRENDIENDO);
+
+/*
+    *************************
+    ************************* 
+    ************************* 
+             Nulos
+    ************************* 
+    ************************* 
+    ************************* 
+*/
+
+let nulo = null;
+
+console.log(nulo);
+
+/*
+    *************************
+    ************************* 
+    ************************* 
+           No definido
+    ************************* 
+    ************************* 
+    ************************* 
+*/
+
+let noDefinido = null;
+
+console.log(noDefinido);
+
+/*
+    *************************
+    ************************* 
+    ************************* 
+      No es un numero (NaN)
+    ************************* 
+    ************************* 
+    ************************* 
+*/
+
+let numero3 = 0/0;
+
+console.log(numero3);
