10af759599866452702b059e34ef48231b9fea4b
carlos <crdgom@gmail.com>
2022-10-27 17:32:43 -0500

    Repaso JS

---
+++ Modulo-2--JavaScript/Repaso-intro-JavaScript/index.js
@@ -0,0 +1,110 @@
+/* /* // repaso del modulo 2 Intro a JavaScript
+
+// Variables
+
+/* function x(a,b){
+    
+}
+
+let personas = 50
+let capacidad = 10
+
+function subirEdificio(numero, elevadores){
+    console.log(numero / elevadores)
+    console.log("la combinación del color " + c1 "con el color " + c2 "es amarillo")
+}
+
+subirEdificio(personas, capacidad) */
+
+/* let primarioAzul = "azul"
+let primarioAmarillo = "amarillo"
+let primarioRojo = "rojo"
+
+function combinacionColores(c1,c2){
+
+    if (c1 === primarioAzul && c2 === primarioAmarillo ){
+        console.log("el color que sale de combinar"  + c1 + "y " + c2 + "es el Verde");
+    }
+    
+    if (c1 === primarioAzul && c2 === primarioRojo ){
+        console.log("el color que sale de combinar "  + c1 + " y " + c2 +" es el Morado");
+    }
+
+}
+
+combinacionColores(primarioAzul, primarioRojo)
+
+ */
+
+// Scope
+
+/* let area1 = "Planeta Tierra"  // varible global
+
+console.log(area1)
+
+
+function ciudad(){
+    let area = "CDMX"
+
+
+    console.log(area + " está dentro de " + area1) */
+
+        /* function colonia(){
+
+            areacol = "Polanco" + areac
+
+            console.log(area)
+
+            retun areacol 
+
+            function casa(){
+                areac = "Goldsmith 49"
+
+                console.log(area)
+                return areac
+            }
+        } */
+
+/* }
+
+ciudad()
+
+
+
+function chocolates(tipo, endulzante){
+    if (tipo === "chocolateConLeche" && endulzante === "Azucar"){
+
+        console.log ("Chocolate rico")
+    }else{
+
+        if (tipo === "chocolateConLeche" && endulzante === "Estevia"){
+            console.log("Endulzante Estevia")
+
+        }else if (tipo === "chocolateConLeche" && endulzante === "Monkfruit") {
+
+            console.log("Endulzante MonkFruit")
+
+        } else if (tipo === "chocolateBlanco" && endulzante === "Monkfruit") {
+            
+            console.log("Endulzante MonkFruit")
+        }{}
+    }
+} */
+
+
+
+function extractInput(){
+
+
+
+        let inputValue = document.getElementById("nombre").value
+
+        console.log(inputValue)
+
+}
+
+for (let i; i= 0; i ++){
+    for (let j; j= 0; j ++){
+
+    }
+}
